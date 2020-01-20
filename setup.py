5d2ae7da437cfd8f9cf351b9602269c115dcd46f
buec <m.buechi@outlook.com>
2020-01-20 14:00:48 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ except FileNotFoundError:
 ##################################################
 
 setup(name='spoteno',
-      version='0.1.0',
+      version='0.1.1',
       description=DESCRIPTION,
       long_description=long_description,
       long_description_content_type='text/markdown',
